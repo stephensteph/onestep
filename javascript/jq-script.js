c136f87d40f2155758488e739f83e8d40ca53e03
stephensteph <stephensteph8@yahoo.com>
2017-06-21 16:10:08 +0000

    New

--- javascript/jq-script.js
+++ javascript/jq-script.js
@@ -1,14 +1,28 @@
-$(document).ready(function () {
-	function GallerySlide() {
+$(document).ready(function(){
 
-		
-		
-	// 	$('div.slideText').animate({
-	// 		opacity: '0',
-	// 		height: '0px',
-	// 	}, "slow");	
-	// }
+  $('.slideshow_container').slick({
+  		arrows: false,
+  		dots: true,
+  		autoplay: true,
+  		draggable: false,
+  });
 
-	// setTimeout(GallerySlide, 2000);
-	}
+  var icons = $('#menu li');
+  var h2 = $(".topNav h2");
+  var tl = new TimelineLite();
+  tl.from(h2, 1.2, {top:-100, opacity:0, autoAlpha:0});
+
+  $('#toggle').click(function(){
+  	$('#menu').fadeIn(1500);
+  	$('.full_container').fadeOut(1500);
+  	tl.to(h2, 5, {top:-100, opacity:0, autoAlpha:0})
+  	.staggerFrom(icons, 1.2, {scale:0, rotation:-360, autoAlpha:0}, 0.3, "-=4");
+  });
+
+  $('#toggle_remove').click(function(){
+  	tl.to(h2, 1.2, {top:0, visibility:'visible', opacity:1, autoAlpha:0});
+  	$('#menu').fadeOut(1500);
+  	$('.full_container').fadeIn(1500);
+  });
+
 });
